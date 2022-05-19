d14e824e2dee023b077aac473c251abc3e979d9e
Felix Reynoso <engfelixreynoso@gmail.com>
2022-05-19 12:03:53 -0700

    modified printLevel to print results

--- printLevel.py
+++ printLevel.py
@@ -1,3 +1,34 @@
+# class Node:
+#     # A utility function to create a new node
+#     def __init__(self, key):
+#         self.data = key
+#         self.left = None
+#         self.right = None
+
+
+# root = Node('A')
+# root.left = Node('B')
+# root.right = Node('C')
+# root.left.left = Node('D')
+# root.left.right = Node('E')
+# root.right.left = Node('F')
+# root.right.right = Node('G')
+
+
+# from collections import defaultdict
+# levels = defaultdict(lambda:[])
+
+
+# def DFSUtil(root, level=0):
+#     if root:
+#         levels[level].append(root.data)
+
+#         if root.left:
+#             DFSUtil(root.left,level+1)
+#         if root.right:
+#             DFSUtil(root.right,level+1)
+
+
 class Node:
     # A utility function to create a new node
     def __init__(self, key):
@@ -27,3 +58,6 @@ def DFSUtil(root, level=0):
             DFSUtil(root.left,level+1)
         if root.right:
             DFSUtil(root.right,level+1)
+
+DFSUtil(root)
+print(levels)
